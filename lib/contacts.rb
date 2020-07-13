ebe4abd40a464870392db4c8d59187bdac3a2f25
James-Ryan Stampley <computersciencewarrior@gmail.com>
2020-07-13 03:32:07 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -20,6 +20,7 @@ def remove_strawberry(contacts)
     if contact == "Freddy Mercury"
       contact_val.each do |attribute, data|
         if attribute == :favorite_ice_cream_flavors
+          data.delete_if {|ice_cream| ice_cream == "strawberry"}
       end
     end
   end
